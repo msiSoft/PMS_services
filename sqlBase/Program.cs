b8d52d397d9f0fdfb4b922994649f13355f46ba6
msi1002 <vins@mediaus.com>
2016-05-09 19:02:51 +0530

    temp code before inset of po values

--- sqlBase/Program.cs
+++ sqlBase/Program.cs
@@ -19,6 +19,24 @@ namespace sqlBase
             Purchase PR = new Purchase();
             PR.GetPONumbers("400.0000000024"); // Get all the PO numbers for the vessel
             PR.GetAllPODetails("400.0000000024"); // Get all the item details for the vessel
+            gettingRdyForInsert();
+
+        }
+        public static void gettingRdyForInsert()
+        {
+            string p_vslcode = "'COMMON'";
+            string vslcode = "400.0000000024";
+            object res;
+            //OperationsOnSourcecDB(" UPDATE PURCHASE.lastcodes SET grv_no = grv_no + 1 WHERE p_vslcode =" +  p_vslcode );
+
+            //OperationsOnSourcecDB("UPDATE PURCHASE.lastcodes SET agrv_no = agrv_no + 1 WHERE p_vslcode = " + vslcode);
+           
+            int grv_no = (int)ExecuteScalarOnSourcecDB("SELECT grv_no FROM PURCHASE.lastcodes WHERE p_vslcode = " + p_vslcode);
+            int agrv_no = (int)ExecuteScalarOnSourcecDB("SELECT agrv_no FROM PURCHASE.lastcodes WHERE p_vslcode =" + vslcode);
+            //string Initial=(string)ExecuteScalarOnSourcecDB("SELECT INITIAL FROM PMS.PMS_VESSELMF WHERE VSLCODE =" + vslcode + " AND P_LATEST = 1 AND UPDFLAG <> 'D'");
+            //string Prefix = (string)ExecuteScalarOnSourcecDB("SELECT GRV_PREFIX FROM PURCHASE.SETUP");
+            string Suffix = (string)ExecuteScalarOnSourcecDB("SELECT GRV_SUFFIX FROM PURCHASE.SETUP");
+
         }
         public static void Method2Ins()
         {
@@ -50,5 +68,25 @@ namespace sqlBase
                 throw;
             }
         }
-    }
+            public static object  ExecuteScalarOnSourcecDB(string qry)
+             {
+                object res;
+                try
+                {
+                    OleDbHelper sourcedb = new OleDbHelper();
+
+                    sourcedb.createCommand();
+                    sourcedb.command.CommandText = qry;
+                    res = sourcedb.command.ExecuteScalar();
+                    sourcedb.commit();
+
+                    sourcedb = null;
+                    return res;
+                }
+                catch
+                {
+                    throw;
+                }
+            }
+        }
 }
