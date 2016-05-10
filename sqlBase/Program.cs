4e734dda82da22bab4ba232d6130341871a5c65f
msi1002 <vins@mediaus.com>
2016-05-10 12:01:11 +0530

    chking if result of exeutescalar is null

--- sqlBase/Program.cs
+++ sqlBase/Program.cs
@@ -26,15 +26,30 @@ namespace sqlBase
         {
             string p_vslcode = "'COMMON'";
             string vslcode = "400.0000000024";
-            object res;
+            string strInitial, strPrefix;
             //OperationsOnSourcecDB(" UPDATE PURCHASE.lastcodes SET grv_no = grv_no + 1 WHERE p_vslcode =" +  p_vslcode );
 
             //OperationsOnSourcecDB("UPDATE PURCHASE.lastcodes SET agrv_no = agrv_no + 1 WHERE p_vslcode = " + vslcode);
-           
+
             int grv_no = (int)ExecuteScalarOnSourcecDB("SELECT grv_no FROM PURCHASE.lastcodes WHERE p_vslcode = " + p_vslcode);
             int agrv_no = (int)ExecuteScalarOnSourcecDB("SELECT agrv_no FROM PURCHASE.lastcodes WHERE p_vslcode =" + vslcode);
-            //string Initial=(string)ExecuteScalarOnSourcecDB("SELECT INITIAL FROM PMS.PMS_VESSELMF WHERE VSLCODE =" + vslcode + " AND P_LATEST = 1 AND UPDFLAG <> 'D'");
-            //string Prefix = (string)ExecuteScalarOnSourcecDB("SELECT GRV_PREFIX FROM PURCHASE.SETUP");
+
+            if (ExecuteScalarOnSourcecDB("SELECT INITIAL FROM PMS.PMS_VESSELMF WHERE VSLCODE =" + vslcode + " AND P_LATEST = 1 AND UPDFLAG <> 'D'") != DBNull.Value)
+            {
+                strInitial = (string)ExecuteScalarOnSourcecDB("SELECT INITIAL FROM PMS.PMS_VESSELMF WHERE VSLCODE =" + vslcode + " AND P_LATEST = 1 AND UPDFLAG <> 'D'");
+            }
+            else
+            {
+                strInitial = "";
+            }
+            if (ExecuteScalarOnSourcecDB("SELECT GRV_PREFIX FROM PURCHASE.SETUP")!=DBNull.Value )
+            {
+                strPrefix= (string)ExecuteScalarOnSourcecDB("SELECT GRV_PREFIX FROM PURCHASE.SETUP");
+            }
+            else
+            {
+                strPrefix ="";
+            }
             string Suffix = (string)ExecuteScalarOnSourcecDB("SELECT GRV_SUFFIX FROM PURCHASE.SETUP");
 
         }
