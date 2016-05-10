de26988b92ba05caaa0f2efb4b2d37bdeff40421
msi1002 <vins@mediaus.com>
2016-05-10 15:51:26 +0530

    added class files

---
+++ sqlBase/Classes/POFinal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace sqlBase.Classes
+{
+    public class POFinal
+    {
+        public string po_number { get; set; }
+        public string item_code { get; set; }
+        public string code_type { get; set; }
+        public decimal requested_qty { get; set; }
+    }
+}
---
+++ sqlBase/Classes/POHdr.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace sqlBase.Classes
+{
+    public class POHdr
+    {
+        public string po_number {get;set;}
+        public string cpo_number { get; set; }
+        public string vd_code { get; set; }
+        public DateTime po_date { get; set; }
+        public int challan_number { get; set; }
+        public  DateTime receipt_date { get; set; }
+        public string remarks { get; set; }
+    }
+}
