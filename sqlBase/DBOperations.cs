86915e4fb8ea77e122a7e9174e19cee6d4eb2d50
msi1002 <vins@mediaus.com>
2016-05-11 18:18:30 +0530

    added new file for dboperations

---
+++ sqlBase/DBOperations.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace sqlBase
+{
+    public class DBOperations
+    {
+        public int OperationsOnSourceDB(string qry)
+        {
+            int res = 0;
+
+            try
+            {
+                OleDbHelper sourcedb = new OleDbHelper();
+
+                sourcedb.createCommand();
+                sourcedb.command.CommandText = qry;
+                res = sourcedb.ExecuteQuery();
+
+                sourcedb.command.CommandText = "COMMIT";
+                res = sourcedb.ExecuteQuery();
+
+                sourcedb.commit();
+                sourcedb = null;
+                return res;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+        public object ExecuteScalarOnSourceDB(string qry)
+        {
+            object res;
+            try
+            {
+                OleDbHelper sourcedb = new OleDbHelper();
+
+                sourcedb.createCommand();
+                sourcedb.command.CommandText = qry;
+                res = sourcedb.command.ExecuteScalar();
+                sourcedb.commit();
+
+                sourcedb = null;
+                return res;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
