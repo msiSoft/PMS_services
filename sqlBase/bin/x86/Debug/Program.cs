c35afa87a78582c33cec263dc8e722917bf11895
jaya.media <jayalakshmi@msisoft.in>
2016-05-19 18:25:03 +0530

    worksdone

--- sqlBase/bin/x86/Debug/Program.cs
+++ sqlBase/bin/x86/Debug/Program.cs
@@ -15,6 +15,9 @@ namespace sqlBase
 
         static void Main(string[] args)
         {
+            
+            string zone= (string)ExecuteScalarOnSourceDB("SELECT ZONE FROM PMS.SETUP");
+
             //ZonesAndEquipments ZE = new ZonesAndEquipments();
             //ZE.GetEquipments("400.0000000024");
             //Common MasterData = new Common();
@@ -25,7 +28,7 @@ namespace sqlBase
             ////PR.ReceiveGoods();
 
 
-            //KeyGenForInsert();
+            KeyGenForInsert();
 
             ////Declaring an object of purchase header class 
             //POHdr purchaseHdr = new POHdr();
@@ -93,40 +96,56 @@ namespace sqlBase
             // RS.GetLastCodes("COMMON");
             // RS.GetIndDt("400.0000000024");
             // RS.GetIndHd("400.0000000024");
-          
-            RequisitionSave RS = new RequisitionSave();
 
-            RS.aid_no = "0";
-            RS.id_number = "0000000009";
-            RS.eq_number = " 43";
-            RS.zone = "UNIX";
-            RS.item_code = "UNIX.00017186";
-            RS.required_qty = "3";
-            RS.deliver_before = "18-MAR-2016 09:34:17";
-            RS.cc_code = "Z";
-            RS.ccm_code = "Z";
-            RS.rob_qty = "1";
-            RS.updflag = "C";
-            RS.code_type = "I";
-            RS.order_no = "1";
-            RS.vessel_code = "400.0000000024";
-            RS.requisition_number = "-16-0007";
-            RS.requisition_date = "18-MAR-2016 09:34:17";
-            RS.id_level = "N";
-            RS.id_reqby = "MASTER";
-            RS.forw_flag = "Z";
-            RS.po_no = "Z";
-            RS.send = "1";
-            RS.tp_code = "004";
-            RS.im_category = "G";
-            RS.dept_code = "7";
-            RS.attachment = "0";
-            RS.fromoff = "V";
+            //RequisitionSave RS = new RequisitionSave();
+
+            //RS.aid_no = "0";
+            //RS.id_no = "2";
+            //RS.eq_number = " 50000";
+
+            //RS.SaveRequisitionUpd(RS);
+            RecieveGoods();
+        }
+        public static void RecieveGoods()
+        {
+            KeyGenForInsert();
+
+            //Declaring an object of purchase header class 
+            POHdr goodsReceivedHdr = new POHdr();
+
+            // Assigning dummy values to purchase header class variables
+            goodsReceivedHdr.po_number = "0000002254";
+            goodsReceivedHdr.cpo_number = " AST002/P16";
+            goodsReceivedHdr.vd_code = "0000000154";
+            goodsReceivedHdr.po_date = "15-MAR-2016";
+            goodsReceivedHdr.challan_number = 1234;
+            goodsReceivedHdr.receipt_date = "15-MAR-2016";
+            goodsReceivedHdr.remarks = "Test";
+            goodsReceivedHdr.data_entered_date = "15-MAR-2016";
+            goodsReceivedHdr.data_entered_by = "SBN";
+            goodsReceivedHdr.is_updated_on_server = true;
+            // goodsReceivedHdr.SavePurchaseHdr(vslcode,grvno_auto, cgrv_no,goodsReceivedHdr);
+            //goodsReceivedHdr.UpdPOHdr(vslcode, goodsReceivedHdr);
+
+            //Declaring an object of purchase header class 
+            POFinal goodsReceivedtl = new POFinal();
+            goodsReceivedtl.po_number = "0000002254";
+            goodsReceivedtl.item_code = "$BNT730102";// "UNIX.00011816";
+            goodsReceivedtl.requested_qty = 10;
+            goodsReceivedtl.ordered_qty = 10;
+            goodsReceivedtl.received_qty = 10;
+            goodsReceivedtl.accepted_qty = 8;
+            goodsReceivedtl.code_type = "I";
+            goodsReceivedtl.data_entered_date = "15-MAR-2016";
+            goodsReceivedtl.data_entered_by = "SBN";
+            goodsReceivedtl.is_updated_on_server = true;
+            // goodsReceivedtl.SavePurchaseDtl(vslcode, grvno_auto, goodsReceivedtl, goodsReceivedHdr.vd_code);
+           // goodsReceivedtl.UpdClosedFlag(goodsReceivedtl);
+            goodsReceivedtl.UpdClosedFlagInPurchaseDtl(goodsReceivedtl);
+            //goodsReceivedtl.UpdStock(goodsReceivedtl, vslcode);
+            
 
 
-            // RS.SaveRequisitionUpd(RS);
-            //RS.SaveRequisitionInsrtDt(RS);
-            RS.SaveRequisitionInsrtHd(RS);
 
         }
 
