cba04f95eb86101ce03b4b49712f56519a10754d
msi1002 <vins@mediaus.com>
2016-05-31 11:12:25 +0530

    modified storeinfo function

--- sqlBase/Common.cs
+++ sqlBase/Common.cs
@@ -184,19 +184,27 @@ namespace sqlBase
         {
             try
             {
-                string qry = @"SELECT       IM_CODE as item_code,
-                                                        CIM_CODE,
-                                            IM_DESC as item_desc,
-                                            IM_SPEC as item_spec,
-                                                        SE2_CODE,
-                                            IM_UNIT as item_unit,
-                                                        MIN_STOC,
-                                                        MAX_STOC,
-                                          CODE_TYPE as code_type,                                       
-                                        DE_BY as data_entered_by,
-                                       DE_AT as data_entered_date                                             
-                                                             FROM    PURCHASE.ITEM_MF 
-                                                            WHERE    UPDFLAG<>'D'";
+                //string qry = @"SELECT       IM_CODE as item_code,
+                //                                        CIM_CODE,
+                //                            IM_DESC as item_desc,
+                //                            IM_SPEC as item_spec,
+                //                                        SE2_CODE,
+                //                            IM_UNIT as item_unit,
+                //                                        MIN_STOC,
+                //                                        MAX_STOC,
+                //                          CODE_TYPE as code_type,                                       
+                //                        DE_BY as data_entered_by,
+                //                       DE_AT as data_entered_date                                             
+                //                                             FROM    PURCHASE.ITEM_MF 
+                //                                            WHERE    UPDFLAG<>'D'";
+
+                string qry = @"SELECT  IM_CODE as item_code,
+                                       IM_DESC as item_desc,
+                                       IM_SPEC as item_spec,
+                                       IM_UNIT as item_unit,
+                                       CODE_TYPE as code_type
+                                                        FROM PURCHASE.ITEM_MF 
+                                                            WHERE UPDFLAG<>'D'";
                 SqlBase_OleDb db = new SqlBase_OleDb(qry);
                 DataTable tbl = db.GetTable();
             }
