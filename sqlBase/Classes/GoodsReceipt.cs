4cd0f35af301a3533f7aa9c1e3465b26fe5d7189
msi1002 <vins@mediaus.com>
2016-06-02 17:56:05 +0530

    "GoodsReceipt" class

--- sqlBase/Classes/GoodsReceipt.cs
+++ sqlBase/Classes/GoodsReceipt.cs
@@ -5,7 +5,30 @@ using System.Text;
 
 namespace sqlBase.Classes
 {
-    class GoodsReceipt
+    public class GoodsReceipt
     {
+        public string vslcode { get; set; }
+        public Dictionary<PurchaseHdrObj , List<ItemsObj>> GR { get; set; }
+    }
+
+    public class PurchaseHdrObj
+    {
+        public string ponumber { get; set; }
+        public string vd_code { get; set; }
+        public string challan_number { get; set; }
+        public string receipt_date { get; set; }        //date
+        public string remarks { get; set; }       
+        public string data_entered_by { get; set; }
+        public string data_entered_at { get; set; }    //date 
+
+    }
+
+    public class ItemsObj
+    {
+        public string ponumber { get; set; }
+        public string item_code  { get; set; }
+        public string code_type { get; set; }
+        public decimal rcvd_qty { get; set; }
+        public decimal accptd_qty { get; set; }
     }
 }
